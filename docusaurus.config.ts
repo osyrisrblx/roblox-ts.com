649a500241dbfbfe503a368ffa9af110c635a3f6
Osyris <osyrisrblx@gmail.com>
2024-02-13 14:30:29 -0500

    Update docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -12,7 +12,7 @@ const config: Config = {
 	url: "https://roblox-ts.com",
 	// Set the /<baseUrl>/ pathname under which your site is served
 	// For GitHub pages deployment, it is often '/<projectName>/'
-	baseUrl: "/",
+	baseUrl: "/roblox-ts.com/",
 
 	// GitHub pages deployment config.
 	// If you aren't using GitHub pages, you don't need these.
